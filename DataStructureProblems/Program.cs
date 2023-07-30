9411e8acbf20177b68438ae368f23b24582d5241
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-30 23:27:49 +0530

    [jogan] Balanced Paranthesis problem implemented

--- DataStructureProblems/Program.cs
+++ DataStructureProblems/Program.cs
@@ -1,15 +1,19 @@
-﻿using DataStructureProblems.OrderedList;
+﻿using DataStructureProblems.BalancedParanthesis;
+using DataStructureProblems.OrderedList;
 using DataStructureProblems.UnorderedList;
 
 public class Program
 {
     static string unorderedFilepath = @"E:\Bridgelabz\DataStructureProblems\DataStructureProblems\DataStructureProblems\UnorderedList\UnorderedList.txt";
     static string orderedFilepath = @"E:\Bridgelabz\DataStructureProblems\DataStructureProblems\DataStructureProblems\OrderedList\OrderedList.txt";
+    static string stackFilepath = @"E:\Bridgelabz\DataStructureProblems\DataStructureProblems\DataStructureProblems\BalancedParanthesis\BalancedParanthesisText.txt";
     private static void Main(string[] args)
     {
         //UnorderedListOperations operations = new UnorderedListOperations();
         //operations.ReadFileAndPerformOperation(unorderedFilepath);
-        OrderedListOperations operations = new OrderedListOperations();
-        operations.ReadAndPerformOperation(orderedFilepath);
+        //OrderedListOperations operations = new OrderedListOperations();
+        //operations.ReadAndPerformOperation(orderedFilepath);
+        BalancedParanthesisOperation operation = new BalancedParanthesisOperation();
+        operation.ReadAndPerformOperation(stackFilepath);
     }
 }
