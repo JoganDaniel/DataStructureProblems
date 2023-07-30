158205683cabca0958058d3187e54175ccbc4b11
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-30 23:03:57 +0530

    [jogan] Unordered list problem implemented

---
+++ DataStructureProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿using DataStructureProblems.UnorderedList;
+
+public class Program
+{
+    static string unorderedFilepath = @"E:\Bridgelabz\DataStructureProblems\DataStructureProblems\DataStructureProblems\UnorderedList\UnorderedList.txt";
+    private static void Main(string[] args)
+    {
+        UnorderedListOperations operations = new UnorderedListOperations();
+        operations.ReadFileAndPerformOperation(unorderedFilepath);
+    }
+}
