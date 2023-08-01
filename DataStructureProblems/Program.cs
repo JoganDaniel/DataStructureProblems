69f0813c22c5d422a21bbc8480f4fae69e973590
Jogan Daniel A <jogan.d@tvsnext.com>
2023-08-01 17:36:12 +0530

    [jogan] Hashing function implemented

--- DataStructureProblems/Program.cs
+++ DataStructureProblems/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DataStructureProblems.BalancedParanthesis;
 using DataStructureProblems.BankingCashCounter;
+using DataStructureProblems.HashTable;
 using DataStructureProblems.OrderedList;
 using DataStructureProblems.UnorderedList;
 
@@ -8,6 +9,7 @@ public class Program
     static string unorderedFilepath = @"E:\Bridgelabz\DataStructureProblems\DataStructureProblems\DataStructureProblems\UnorderedList\UnorderedList.txt";
     static string orderedFilepath = @"E:\Bridgelabz\DataStructureProblems\DataStructureProblems\DataStructureProblems\OrderedList\OrderedList.txt";
     static string stackFilepath = @"E:\Bridgelabz\DataStructureProblems\DataStructureProblems\DataStructureProblems\BalancedParanthesis\BalancedParanthesisText.txt";
+    static string hashFilePath = @"E:\Bridgelabz\DataStructureProblems\DataStructureProblems\DataStructureProblems\HashTable\Hashtable.txt";
     private static void Main(string[] args)
     {
         //UnorderedListOperations operations = new UnorderedListOperations();
@@ -16,7 +18,10 @@ public class Program
         ////operations.ReadAndPerformOperation(orderedFilepath);
         //BalancedParanthesisOperation operation = new BalancedParanthesisOperation();
         //operation.ReadAndPerformOperation(stackFilepath);
-        BankingOperations operations = new BankingOperations(10000);
-        operations.AddPersonsInQueue();
+        //BankingOperations operations = new BankingOperations(10000);
+        //operations.AddPersonsInQueue();
+        HashingOperations operations = new HashingOperations();
+        operations.ReadFile(hashFilePath);
+        operations.Display();
     }
 }
